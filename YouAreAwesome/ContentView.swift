9eb0f1cc5b4014298c96de0f76a4e9943350a5c4
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-08-31 19:23:53 -0400

    Update after first lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        // VStack allows for all views inside of it to be stacked VERTICALLY
+        // HStack allows for all views inside of it to be stacked HORIZONTALLY
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(Color.orange)
+            //Text() creates a Text interface known as a View. You can pass a String of characters between these parentheses
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.red)
         }
         .padding()
+        
     }
 }
 
