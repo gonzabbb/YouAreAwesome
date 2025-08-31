7ccb25539f364bb1336c007c35722236ceb67175
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-08-31 18:15:43 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Abraham Gonzalez on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
