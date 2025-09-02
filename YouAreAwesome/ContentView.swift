e4c25ae5a18504ceff486f059e11ec22a30e0093
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-09-01 22:36:15 -0400

    After lesson 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var  message = "I Am a Developer!"
+    @State private var  message = ""
+    @State private var image = ""
     var body: some View {
         
-        // VStack allows for all views inside of it to be stacked VERTICALLY
-        // HStack allows for all views inside of it to be stacked HORIZONTALLY
+        
         
         VStack {
             
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: image)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(Color.orange)
                 .frame(width: 200, height: 200)
             
             
-            //Text() creates a Text interface known as a View. You can pass a String of characters between these parentheses
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.ultraLight)
@@ -32,16 +32,23 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack{
-                Button("Awesome!") {
-                    message = "Awesome!"
+            
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let image1 = "hand.thumbsup"
+                let image2 = "sun.max.fill"
+                if message == message1 {
+                    message = message2
+                    image = image1
                 }
-                
-                
-                Button("Great!") {
-                    message = "Great!"
+                else {
+                    message = message1
+                    image = image2
                 }
             }
+            
+            
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .tint(.orange)
