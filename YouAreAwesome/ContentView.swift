accaab73e5b814501d9b576cea0af71f31bc26f0
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-09-06 16:30:30 -0400

    After Lesson 1.10

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -18,14 +18,6 @@ struct ContentView: View {
         
         VStack {
             
-            Spacer()
-            Image(image)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius:30)
-                .frame(width: 200, height: 200)
-            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -35,21 +27,35 @@ struct ContentView: View {
                     default: return Color.red
                     }
                 }())
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+
             
             Spacer()
             
+            Image(image)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius:30)
+                .frame(width: 200, height: 200)
+                .animation(.default, value: image)
+            
+            Spacer()
             
             Button("Show Message") {
-                let message0 = "Yankees!"
-                let message1 = "Red Sox!"
-                let message2 = "Orioles!"
-                let message3 = "Blue Jays!"
-                let message4 = "Rays!"
-                let message5 = "Mets!"
-                let message6 = "Marlins!"
-                let message7 = "Phillies!"
-                let message8 = "Nationals!"
-                let message9 = "Guardians!"
+                let message0 = "Yankees —The kings of baseball with 27 rings, and yes, everyone else is chasing us"
+                let message1 = "Red Sox — Defined mostly by their obsession with beating the Yankees (sometimes it works)"
+                let message2 = "Orioles — Cute little up-and-comers who still get nervous when they visit the Bronx"
+                let message3 = "Blue Jays — Canada’s best at baseball… but that’s a pretty short list"
+                let message4 = "Rays — A team that wins games in front of tens of fans and thousands of empty seats"
+                let message5 = "Mets – Our noisy neighbors who swear “this year is different” every year."
+                let message6 = "Marlins – Have two World Series titles but barely enough fans to fill a subway car."
+                let message7 = "Phillies – Tough city, loud fans, and they still hate us because we beat them in 2009."
+                let message8 = "Nationals – Won one title and then disappeared faster than their star players."
+                let message9 = "Guardians – Known for great pitching, but their bats usually fall asleep in October."
                 
                 let messageList = [message0,
                                    message1,
