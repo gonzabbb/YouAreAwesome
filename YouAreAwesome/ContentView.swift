e8c1075d4cd344b90ecf452a1af782996aa945cc
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-09-07 15:09:57 -0400

    Adter Lesson 1.13

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var  message = ""
     @State private var image = ""
+    @State private var sound = ""
     @State private var lastMessageNumber = 0
     @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    let numberOfImages = 10
+    let numberOfSounds = 6
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         
         
@@ -42,14 +49,23 @@ struct ContentView: View {
             
             Button("Show Message") {
                 let message0 = "Yankees —The kings of baseball with 27 rings, and yes, everyone else is chasing us"
+                
                 let message1 = "Red Sox — Defined mostly by their obsession with beating the Yankees (sometimes it works)"
+                
                 let message2 = "Orioles — Cute little up-and-comers who still get nervous when they visit the Bronx"
+                
                 let message3 = "Blue Jays — Canada’s best at baseball… but that’s a pretty short list"
+                
                 let message4 = "Rays — A team that wins games in front of tens of fans and thousands of empty seats"
+                
                 let message5 = "Mets – Our noisy neighbors who swear “this year is different” every year."
+                
                 let message6 = "Marlins – Have two World Series titles but barely enough fans to fill a subway car."
+                
                 let message7 = "Phillies – Tough city, loud fans, and they still hate us because we beat them in 2009."
+                
                 let message8 = "Nationals – Won one title and then disappeared faster than their star players."
+                
                 let message9 = "Guardians – Known for great pitching, but their bats usually fall asleep in October."
                 
                 let messages = [message0,
@@ -73,14 +89,30 @@ struct ContentView: View {
                 
                 var imageNumber: Int
                 repeat {
-                    imageNumber = Int.random(in: 0...9)
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
                 } while imageNumber == lastImageNumber
                 image = "image\(imageNumber)"
                 lastImageNumber = imageNumber
                 
+                var soundNumber: Int
+                repeat {
+                    soundNumber = Int.random(in: 0...(numberOfSounds-1))
+                    guard let soundFile = NSDataAsset(name: "sound\(soundNumber)") else {
+                        print("😡 Could not read file named sound\(soundNumber)")
+                        return
+                    }
+                    
+                    do {
+                        audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                        audioPlayer.play()
+                    } catch {
+                        print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                    }
+                } while lastSoundNumber == soundNumber
+                sound = "sound\(soundNumber)"
+                lastSoundNumber = soundNumber
+                
             }
-            
-            
             .buttonStyle(.borderedProminent)
             .font(.title2)
         }
